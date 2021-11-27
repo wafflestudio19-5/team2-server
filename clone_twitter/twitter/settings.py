0d2a395e9d1b06e6b42eb3b8f19b02ebb3a6aacc
ding-young <lsyhime@snu.ac.kr>
2021-11-27 11:57:08 +0900

    Feat: Add swagger settings

--- clone_twitter/twitter/settings.py
+++ clone_twitter/twitter/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'rest_framework_jwt',
     'rest_framework.authtoken',
     'user',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- clone_twitter/twitter/urls.py
+++ clone_twitter/twitter/urls.py
@@ -17,11 +17,37 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
 from . import settings
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('user.urls')),
 ]
+
+#swagger related url
+schema_url_patterns = [   # urls that we want to check in swagger
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('user.urls')),
+]
+
+schema_view_v1 = get_schema_view(
+    openapi.Info(
+        title="Waffle Team2 project", default_version='v1', description="Team2 clone-twitter API Server",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+    patterns=schema_url_patterns,
+)
+
+urlpatterns += [
+    url(r'^swagger/$', schema_view_v1.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view_v1.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
+]
+
+
 
 if settings.DEBUG_TOOLBAR:
     import debug_toolbar
