a714023cc7bd5d89f69a8d99e41ff31d997773fc
EC2 Default User <ec2-user@ip-172-31-3-69.ap-northeast-2.compute.internal>
2021-11-27 04:34:56 +0000

    Feat: Add nginx, wsgi setting and auto-deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+source ~/.bash_profile
+git pull origin master:master
+cd clone_twitter
+source /home/ec2-user/venv/bin/activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py check --deploy
+sudo pkill gunicorn
+gunicorn twitter.wsgi:application --bind 0.0.0.0:8000 --daemon
+
+sudo systemctl restart nginx
