39f2ccd95003e14c3a3fc6268a0c161170385934
ding-young <lsyhime@snu.ac.kr>
2021-11-27 11:46:05 +0900

    Feat: Add ping-pong api

--- clone_twitter/twitter/urls.py
+++ clone_twitter/twitter/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls import url
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('user.urls')),
 ]
+
+if settings.DEBUG_TOOLBAR:
+    import debug_toolbar
+
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+        path('api-auth/', include('rest_framework.urls')),
+    ]
--- clone_twitter/user/views.py
+++ clone_twitter/user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.views import Response, APIView
+import os
 
 # Create your views here.
+
+class PingPongView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    def get(self, request):
+        return Response(data={'ping': 'pong'}, status=status.HTTP_200_OK)
+
