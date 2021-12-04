a3f60866fddd9cd583aea986d4ed719443bc50fa
ding-young <lsyhime@snu.ac.kr>
2021-12-04 16:56:44 +0900

    Fix: url routing and install pillow

--- clone_twitter/user/urls.py
+++ clone_twitter/user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from user.views import PingPongView
+from user.views import PingPongView, EmailSignUpView
 
 urlpatterns = [
     path('ping/', PingPongView.as_view(), name='ping'),  # /api/v1/ping/
