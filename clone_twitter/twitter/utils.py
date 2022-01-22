41fc67b3db7e9c56fa4b3ef46eb51115660e5a95
ding-young <lsyhime@snu.ac.kr>
2022-01-22 16:29:58 +0900

    Feat: settings for email verification

--- clone_twitter/twitter/utils.py
+++ clone_twitter/twitter/utils.py
@@ -1,7 +1,8 @@
 import os
 from random import randint
 import uuid
-
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+import six
 from django.utils.timezone import now
 
 
@@ -29,3 +30,12 @@ def media_directory_path(instance, filename, usage='tweet'):
     filename_base, filename_ext = os.path.splitext(filename)
     return usage+'/'+now().strftime('%Y%m%d_%H%M%S')+'_'+str(randint(10000000,99999999))+filename_ext
 
+
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (six.text_type(user.pk) + six.text_type(timestamp)) +  six.text_type(user.is_active)
+
+account_activation_token = AccountActivationTokenGenerator()
+
+def active_message(domain, uidb64, token):
+    return f"아래 링크를 클릭하면 회원가입 인증이 완료됩니다.\n\n 회원가입링크 : http://{domain}/user/account/activate/{uidb64}/{token}\n\n감사합니다."
