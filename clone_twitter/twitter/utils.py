29d0f2ebb82e3565a7b42ab08030757ba8d64dd9
ding-young <lsyhime@snu.ac.kr>
2022-01-28 22:06:10 +0900

    Feat: modifiy celery setting

--- clone_twitter/twitter/utils.py
+++ clone_twitter/twitter/utils.py
@@ -39,4 +39,4 @@ class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
 account_activation_token = AccountActivationTokenGenerator()
 
 def active_message(domain, uidb64, token):
-    return f"아래 링크를 클릭하면 회원가입 인증이 완료됩니다.\n\n 회원가입링크 : http://{domain}/user/account/activate/{uidb64}/{token}\n\n감사합니다."
+    return f"아래 링크를 클릭하면 회원가입 인증이 완료됩니다.\n\n 회원가입링크 : http://{domain}/api/v1/verification/email/activate/{uidb64}/{token}\n\n감사합니다."
