a8538f324662ee10de4b434d8edd568b898485d7
ding-young <88336128+ding-young@users.noreply.github.com>
2022-01-28 18:44:08 +0900

    Chore: Delete task.py

--- clone_twitter/user/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-from django.core.mail import EmailMessage
-
-@shared_task
-def send_email_task(mail_title, message_data, mail_to):
-    email = EmailMessage(mail_title, message_data, to=[mail_to])
-    sent_message_count = email.send()
-    print(sent_message_count)
-    return sent_message_count
